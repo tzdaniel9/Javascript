aea680a39c9163468bd3a802c01bdee804c6cbf0
dany <dzingozi@gmail.com>
2025-01-03 22:36:11 +0200

    friend or foe

---
+++ friendOrFoe.js
@@ -0,0 +1,16 @@
+/**
+ * Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+ */
+
+function friend(friends){
+    const result = friends.filter((result => result.length === 4))
+    return result
+  }
+
+  names = ["Ryan", "Kieran", "Jason", "Yous"]
+  console.log(friend(names))
+
+  foes = ["Peter", "Stephen", "Joe"]
+  console.log(friend(foes))
