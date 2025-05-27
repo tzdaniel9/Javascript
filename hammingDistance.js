5be96f95306204881a01439012eb641c4490447e
dany <dzingozi@gmail.com>
2025-05-27 20:18:08 +0200

    further string manipulation datastructures

--- hammingDistance.js
+++ hammingDistance.js
@@ -1,16 +1,46 @@
-function hammingDistance(x, y){
-    if(x.length !== y.length){
-        console.log('Strings must be of the same length')
-    }
+function hammingDistance(x, y) {
+  if (x.length !== y.length) {
+    console.log("Strings must be of the same length");
+  }
 
-    let distance = 0
+  let distance = 0;
 
-    for(let i = 0; i < x.length; i++){
-        if(x[i] !== y[i]){
-            distance++
-        }
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] !== y[i]) {
+      distance++;
     }
-    return distance
+  }
+  return distance;
 }
 
-console.log(hammingDistance("hello", "h3ll0"))
+console.log(hammingDistance("hello", "h3ll0"));
+
+//The hamming distance between two integers is the number of positions at which the
+// corresponding bits are different
+
+//Given two integers x and y, return the hamming distance between them
+
+function hammingDistanceNum(x, y) {
+  x = x.toString(2);
+  y = y.toString(2);
+
+  if (x.length < y.length) {
+    while (x.length !== y.length) x = "0" + x;
+    // console.log('y is '+y.length)
+  } else {
+    while (x.length !== y.length) y = "0" + y;
+    // console.log('x is '+ x.length)
+  }
+
+  let distance = 0;
+
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] !== y[i]) {
+      distance++;
+    }
+  }
+  return distance;
+}
+
+console.log(hammingDistanceNum(1, 4));
+console.log(hammingDistanceNum(4, 1));
---
+++ validAnagram.js
@@ -0,0 +1,14 @@
+// An anagram is a word or phrase formed by rearranging the letters of a different word
+// or phrase, using all the original letters exactly once
+
+//Input: (s = 'anagram'), (t = 'nagaram'); ----->>>>> output: true
+//Input: (s = 'rat'), (t = 'car'); ----->>>>> output: false
+
+const isAnagram = ((s,t) => {
+    s = s.split("").sort().join("")
+    t = t.split("").sort().join("")
+
+    return s === t
+})
+
+console.log(isAnagram('anagram', 'nagaram'))
