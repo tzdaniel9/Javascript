4273c2f8c1a57dc76ab2017dd3aba1cc24ec495d
dany <dzingozi@gmail.com>
2025-06-05 20:16:44 +0200

    tut

---
+++ counter.js
@@ -0,0 +1,2 @@
+// Given an integer n, return a counter function. This counter function initially returns n and then 
+// return 1 more than the previous value every subsequent time it is called (n, n+1, n+2...)
